c61351dfd7e267cf71cedbcb5c5f13855234b701
Bartek Kruszczynski <bartek@codequest.com>
2014-08-05 01:20:41 +0200

    fix a test

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804204700) do
+ActiveRecord::Schema.define(version: 20140804231321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
+    t.string   "email",               default: "",    null: false
+    t.string   "encrypted_password",  default: "",    null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,  null: false
+    t.integer  "sign_in_count",       default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140804204700) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.boolean  "admin",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -23,7 +23,7 @@ describe Users::OmniauthCallbacksController, type: :controller do
     end
     it 'redirects to index' do
       get :google_oauth2
-      expect(response).to redirect_to(root_path)
+      expect(response).to redirect_to(users_dashboard_path)
     end
     it 'creates a user' do
       expect {
