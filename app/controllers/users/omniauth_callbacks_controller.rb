bc0ccf4bcfab93b8e38759a898bd995579294faa
Bartek Kruszczynski <bartek@codequest.com>
2014-08-05 01:12:51 +0200

    add more to travis.yml

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, kind: 'Google')
     else
-      redirect_to users_dashboard_path, alert: 'You must have an email at code quest'
+      redirect_to root_path, alert: 'You must have an email at code quest'
     end
   end
 end
