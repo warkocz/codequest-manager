0b3c7bc0dc9cc6a8d11b9a7cb20bed3decceaf86
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-08-24 18:22:59 +0200

    Test CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 require 'shoulda-callback-matchers'
 
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
