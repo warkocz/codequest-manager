732ce2f3b6c101b43ff463f938bb1280fea2cb1d
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-09-14 21:44:21 +0200

    Move loads of buttons logic to the decorator

--- app/decorators/dish_decorator.rb
+++ app/decorators/dish_decorator.rb
@@ -4,4 +4,18 @@ class DishDecorator < Draper::Decorator
   def belongs_to?(user)
     object.user == user
   end
+
+  def table_buttons(current_user)
+    return if object.order.delivered?
+    buttons = ''
+    if belongs_to?(current_user) && order.in_progress?
+      buttons += h.link_to 'Edit', h.edit_order_dish_path(order, object), class: 'margin-right-small'
+      if belongs_to?(current_user)
+        buttons += h.link_to 'Delete', h.order_dish_path(order, object), data: {confirm: 'Are you sure?'}, method: :delete
+      end
+    else
+      buttons += h.link_to 'Copy', h.copy_order_dish_path(order, object), data: {confirm: 'This will overwrite your current dish! Are you sure?'}
+    end
+    buttons.html_safe
+  end
 end
--- spec/decorators/dish_decorator_spec.rb
+++ spec/decorators/dish_decorator_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe DishDecorator do
   before do
     @user = create(:user)
+    @order = build(:order) do |order|
+      order.user = @user
+    end
+    @order.save
     @dish = build(:dish) do |dish|
       dish.user = @user
+      dish.order = @order
     end
     @dish.save!
     @dish = @dish.decorate
@@ -24,4 +29,27 @@ describe DishDecorator do
     end
   end
 
+  describe '#table_buttons' do
+    it 'returns nil when order is delevered' do
+      @order.delivered!
+      expect(@dish.table_buttons(@user)).to be_nil
+    end
+    it 'returns adequate when dish belongs to a different_user' do
+      @other_user = create :other_user
+      expected = '<a data-confirm="This will overwrite your current dish! Are you sure\?" href="\/orders\/.*?\/dishes\/.*?\/copy">Copy</a>'
+      expect(@dish.table_buttons(@other_user)).to match(expected)
+    end
+    describe 'with edit button' do
+      before do
+        @expected = '<a class="margin-right-small" href="\/orders\/.*?\/dishes\/.*?\/edit">Edit</a>'
+      end
+      it 'returns adequte when dish belongs to current_user and not in progress' do
+        expect(@dish.table_buttons(@user)).to match(@expected)
+      end
+      it 'returns adequte when dish belongs to current_user and in progress' do
+        @expected += '<a data-confirm="Are you sure\?" data-method="delete" href="\/orders\/.*?\/dishes\/.*?" rel="nofollow">Delete</a>'
+        expect(@dish.table_buttons(@user)).to match(@expected)
+      end
+    end
+  end
 end
