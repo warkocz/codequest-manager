3e718565c536ce846b7aa3eb8f9454f7fe0ef093
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-09-17 20:33:48 +0200

    First attempt at decorator refactor

--- app/decorators/dish_decorator.rb
+++ app/decorators/dish_decorator.rb
@@ -8,12 +8,12 @@ class DishDecorator < Draper::Decorator
   def table_buttons(current_user)
     return if object.order.delivered?
     buttons = ''
-    if belongs_to?(current_user) && order.in_progress?
+    if belongs_to?(current_user) && !order.delivered?
       buttons += h.link_to 'Edit', h.edit_order_dish_path(order, object), class: 'margin-right-small'
-      if belongs_to?(current_user)
-        buttons += h.link_to 'Delete', h.order_dish_path(order, object), data: {confirm: 'Are you sure?'}, method: :delete
-      end
-    else
+    end
+    if belongs_to?(current_user) && order.in_progress?
+      buttons += h.link_to 'Delete', h.order_dish_path(order, object), data: {confirm: 'Are you sure?'}, method: :delete
+    elsif order.in_progress?
       buttons += h.link_to 'Copy', h.copy_order_dish_path(order, object), data: {confirm: 'This will overwrite your current dish! Are you sure?'}
     end
     buttons.html_safe
