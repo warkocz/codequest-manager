b06feb88385d77f058379e3a86397b358b3bfe88
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-09-17 21:22:21 +0200

    Very large dish decorator refactor

--- app/decorators/dish_decorator.rb
+++ app/decorators/dish_decorator.rb
@@ -1,21 +1,31 @@
 class DishDecorator < Draper::Decorator
+  include Draper::LazyHelpers
+  
   delegate_all
 
-  def belongs_to?(user)
-    object.user == user
+  def belongs_to_current_user?
+    object.user == current_user
   end
 
-  def table_buttons(current_user)
-    return if object.order.delivered?
-    buttons = ''
-    if belongs_to?(current_user) && !order.delivered?
-      buttons += h.link_to 'Edit', h.edit_order_dish_path(order, object), class: 'margin-right-small'
+  def table_buttons
+    edit_button.to_s + delete_button.to_s + copy_button.to_s
+  end
+
+  def edit_button
+    if belongs_to_current_user? && !order.delivered?
+      link_to 'Edit', edit_order_dish_path(order, object), class: 'margin-right-small'
     end
-    if belongs_to?(current_user) && order.in_progress?
-      buttons += h.link_to 'Delete', h.order_dish_path(order, object), data: {confirm: 'Are you sure?'}, method: :delete
-    elsif order.in_progress?
-      buttons += h.link_to 'Copy', h.copy_order_dish_path(order, object), data: {confirm: 'This will overwrite your current dish! Are you sure?'}
+  end
+
+  def delete_button
+    if belongs_to_current_user? && order.in_progress?
+      link_to 'Delete', order_dish_path(order, object), data: {confirm: 'Are you sure?'}, method: :delete
     end
-    buttons.html_safe
+  end
+
+  def copy_button
+    if !belongs_to_current_user? && order.in_progress?
+      link_to 'Copy', copy_order_dish_path(order, object), data: {confirm: 'This will overwrite your current dish! Are you sure?'}
+    end
   end
 end
--- spec/decorators/dish_decorator_spec.rb
+++ spec/decorators/dish_decorator_spec.rb
@@ -13,42 +13,98 @@ describe DishDecorator do
     end
     @dish.save!
     @dish = @dish.decorate
+    allow(@dish).to receive(:current_user).and_return(@user)
+    @other_user = create :other_user
   end
 
-  describe '#belongs_to?' do
-    before do
-      @other_user = create(:other_user)
-    end
-
+  describe '#belongs_to_current_user?' do
     it 'returns false when users differ' do
-      expect(@dish.belongs_to?(@user)).to be_truthy
+      expect(@dish.belongs_to_current_user?).to be_truthy
     end
 
     it 'returns true when users do not differ' do
-      expect(@dish.belongs_to?(@other_user)).to be_falsey
+      allow(@dish).to receive(:current_user).and_return(@other_user)
+      expect(@dish.belongs_to_current_user?).to be_falsey
     end
   end
 
   describe '#table_buttons' do
-    it 'returns nil when order is delevered' do
+    it 'calls necessary methods' do
+      expect(@dish).to receive(:edit_button).and_return('edit_button')
+      expect(@dish).to receive(:delete_button).and_return('delete_button')
+      expect(@dish).to receive(:copy_button).and_return('copy_button')
+      expect(@dish.table_buttons).to eq('edit_buttondelete_buttoncopy_button')
+    end
+    it 'works well with nils' do
+      expect(@dish).to receive(:edit_button).and_return(nil)
+      expect(@dish).to receive(:delete_button).and_return(nil)
+      expect(@dish).to receive(:copy_button).and_return(nil)
+      expect(@dish.table_buttons).to eq('')
+    end
+  end
+
+  describe '#edit_button' do
+    it 'returns nil when dish belongs to different user' do
+      allow(@dish).to receive(:current_user).and_return(@other_user)
+      expect(@dish.edit_button).to be_nil
+    end
+    it 'returns nil when order is delivered' do
       @order.delivered!
-      expect(@dish.table_buttons(@user)).to be_nil
+      expect(@dish.edit_button).to be_nil
     end
-    it 'returns adequate when dish belongs to a different_user' do
-      @other_user = create :other_user
-      expected = '<a data-confirm="This will overwrite your current dish! Are you sure\?" href="\/orders\/.*?\/dishes\/.*?\/copy">Copy</a>'
-      expect(@dish.table_buttons(@other_user)).to match(expected)
-    end
-    describe 'with edit button' do
+    describe 'otherwise' do
       before do
         @expected = '<a class="margin-right-small" href="\/orders\/.*?\/dishes\/.*?\/edit">Edit</a>'
       end
-      it 'returns adequte when dish belongs to current_user and not in progress' do
-        expect(@dish.table_buttons(@user)).to match(@expected)
+      it 'returns button when order is in_progress' do
+        expect(@dish.edit_button).to match(@expected)
       end
-      it 'returns adequte when dish belongs to current_user and in progress' do
-        @expected += '<a data-confirm="Are you sure\?" data-method="delete" href="\/orders\/.*?\/dishes\/.*?" rel="nofollow">Delete</a>'
-        expect(@dish.table_buttons(@user)).to match(@expected)
+      it 'returns button when order is ordered' do
+        expect(@dish.edit_button).to match(@expected)
+      end
+    end
+  end
+
+  describe '#delete_button' do
+    it 'returns nil when dish belongs to different user' do
+      allow(@dish).to receive(:current_user).and_return(@other_user)
+      expect(@dish.delete_button).to be_nil
+    end
+    it 'returns button when order is in_progress' do
+      expected = '<a data-confirm="Are you sure\?" data-method="delete" href="\/orders\/.*?\/dishes\/.*?" rel="nofollow">Delete</a>'
+      expect(@dish.delete_button).to match(expected)
+    end
+    describe 'otherwise' do
+      it 'returns nil when order is ordered' do
+        @order.ordered!
+        expect(@dish.delete_button).to be_nil
+      end
+      it 'returns nil when order is delivered' do
+        @order.delivered!
+        expect(@dish.delete_button).to be_nil
+      end
+    end
+  end
+
+  describe '#copy_button' do
+    describe 'order is in_progress' do
+      it 'returns nil if dish belongs to current user' do
+        expect(@dish.copy_button).to be_nil
+      end
+      it 'returns button when not' do
+        allow(@dish).to receive(:current_user).and_return(@other_user)
+        expected = '<a data-confirm="This will overwrite your current dish! Are you sure\?" href="\/orders\/.*?\/dishes\/.*?\/copy">Copy</a>'
+        expect(@dish.copy_button).to match(expected)
+      end
+    end
+    describe 'otherwise' do
+      it 'returns nil when order is ordered' do
+        @order.ordered!
+        expect(@dish.copy_button).to be_nil
+      end
+      it 'returns nil when order is delivered' do
+        @order.delivered!
+        expect(@dish.copy_button).to be_nil
       end
     end
   end
