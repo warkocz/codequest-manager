becf467a2a37c195d9b874dbd129dbfe3748232a
Bartek Kruszczynski <bartek@kruszczynski.net>
2014-10-02 22:56:27 +0200

    Remove Mailers for now

--- app/mailers/user_notifier.rb
+++
@@ -1,9 +0,0 @@
-class UserNotifier < ActionMailer::Base
-  default from: 'no-reply@codequest-manager.herokuapp.com'
-
-  def debt_notification(user)
-    # @user = user
-    # mail( :to => @user.email,
-    #       :subject => 'Thanks for signing up for our amazing app' )
-  end
-end
